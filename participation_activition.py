b2a776efe7aef2144fa2a01983dc0ca3a65b7b0b
JonD <sethdotse05@gmail.com>
2026-06-14 22:51:22 -0400

    Add purpose of the program in the participation_activity.py file and created the first values

---
+++ participation_activition.py
@@ -0,0 +1,10 @@
+# This program makes sandwiches from a list of orders
+# moves them to finished list
+# Jonathan Dotse, 06/13/2026
+
+# Defining values
+
+sandwich_orders = ['Reuben', 'Cheesesteak', 'Melt', 'Panini', 'Clubmarine', 'Croque Monsieur']
+finished_sandwihes = []
+
+#
